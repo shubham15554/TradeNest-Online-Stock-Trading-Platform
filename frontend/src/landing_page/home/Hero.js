0160ae9e7c54a032390be3bb9121851fd8a7d792
shubham <ry957933@gmail.com>
2025-12-16 15:14:52 +0530

    changed the signup button logic

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,14 @@
 import React from 'react';
- 
+import { Navigate, useNavigate } from 'react-router-dom';
+
 
 function Hero() {
+
+  navigate = useNavigate();
+
+  function handleSignup(){
+    navigate("/signup");
+  }
     return ( 
 
         <div className='container p-5 mb-5'>
@@ -9,7 +16,7 @@ function Hero() {
               <img src='media/images/homeHero.png' className='mb-5' alt='Hero image'></img>
               <h1 className='mt-5'>Invest in everything</h1>
               <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-              <button className='btn btn-primary p-2 fs-5' style={{width:"20%", margin: '0 auto'}}>SignUp for free</button>
+              <button className='btn btn-primary p-2 fs-5' style={{width:"20%", margin: '0 auto'}} onClick={handleSignup}>SignUp for free</button>
           </div>
         </div>
      );
